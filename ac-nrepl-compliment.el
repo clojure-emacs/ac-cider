68c0dc83005ad333ec4eec9d40481260adebe1a8
Alexander Yakushev <alex@bytopia.org>
2014-01-26 00:04:25 +0100

    Rename project to 'ac-cider-compliment'

--- ac-nrepl-compliment.el
+++ ac-nrepl-compliment.el
@@ -1,15 +1,15 @@
-;;; ac-nrepl-compliment.el --- auto-complete sources for Clojure using
-;;; nrepl and compliment
+;;; ac-cider-compliment.el --- auto-complete sources for Clojure using
+;;; CIDER and compliment
 
-;; Copyright (C) 2012-2013 Alex Yakushev <alex@bytopia.org>
+;; Copyright (C) 2012-2014 Alex Yakushev <alex@bytopia.org>
 
 ;; Author: Alex Yakushev <alex@bytopia.org>
 ;;         Steve Purcell <steve@sanityinc.com>
 ;;         Sam Aaron <samaaron@gmail.com>
 ;;
-;; URL: https://github.com/alexander-yakushev/ac-nrepl-compliment
-;; Keywords: languages, clojure, nrepl, compliment
-;; Version: DEV
+;; URL: https://github.com/alexander-yakushev/ac-cider-compliment
+;; Keywords: languages, clojure, nrepl, cider, compliment
+;; Version: 0.1.0
 ;; Package-Requires: ((cider "0.5.0") (auto-complete "1.4"))
 
 ;; This program is free software; you can redistribute it and/or
@@ -28,26 +28,24 @@
 ;;; Commentary:
 
 ;; Provides a number of auto-complete sources for Clojure projects
-;; using nrepl and compliment. This is a fork of ac-nrepl project by
-;; Steve Purcell, with the difference that ac-nrepl-compliment which
-;; uses compliment as completion provider.
+;; using CIDER and compliment. This is a fork of ac-nrepl project by
+;; Steve Purcell, with the difference that ac-cider-compliment uses
+;; Compliment as a completion provider.
 
 ;;; Installation:
 
-;; Currently the only way of installation is to put
-;; ac-nrepl-compliment.el somewhere into ~/.emacs.d/ directory and
-;; load it from there. Installation using package manager is coming
-;; soon.
+;; Available as a package in melpa.milkbox.net.
+;; M-x package-install ac-cider-compliment
 
 ;;; Usage:
 
 ;;     (load "path/to/ac-nrepl-compliment.el")
-;;     (require 'ac-nrepl-compliment)
-;;     (add-hook 'cider-mode-hook 'ac-nrepl-compliment-setup)
+;;     (require 'ac-cider-compliment)
+;;     (add-hook 'cider-mode-hook 'ac-cider-compliment-setup)
 ;;     (eval-after-load "auto-complete"
-;;       '(add-to-list 'ac-modes 'cider-mode))
+;;       '(add-to-list 'ac-modes 'cider-repl-mode))
 
-;; If you want to trigger auto-complete using TAB in nrepl buffers, you may
+;; If you want to trigger auto-complete using TAB in CIDER buffers, you may
 ;; want to use auto-complete in your `completion-at-point-functions':
 
 ;;     (defun set-auto-complete-as-completion-at-point-function ()
@@ -56,52 +54,52 @@
 ;;
 ;;     (add-hook 'cider-mode-hook 'set-auto-complete-as-completion-at-point-function)
 ;;
-;; You might consider using ac-nrepl-compliment's popup documentation in place of `nrepl-doc':
+;; You might consider using ac-cider-compliment's popup documentation in place of `nrepl-doc':
 ;;
-;;     (define-key nrepl-interaction-mode-map (kbd "C-c C-d") 'ac-nrepl-compliment-popup-doc)
+;;     (define-key nrepl-interaction-mode-map (kbd "C-c C-d") 'ac-cider-compliment-popup-doc)
 
 ;;; Code:
 
 (require 'cider)
 (require 'auto-complete)
 
-(defvar ac-nrepl-compliment-core-required ())
+(defvar ac-cider-compliment-core-required ())
 
-(defun ac-nrepl-compliment-require-core ()
+(defun ac-cider-compliment-require-core ()
   (let* ((t-session (nrepl-current-tooling-session))
-        (status (cdr (assoc t-session ac-nrepl-compliment-core-required))))
+        (status (cdr (assoc t-session ac-cider-compliment-core-required))))
     (if status
         status
       (let ((status
              (if (string= (plist-get
-                           (ac-nrepl-compliment-sync-eval
+                           (ac-cider-compliment-sync-eval
                             "(try (require 'compliment.core) (catch Exception e :error))")
                            :value)
                           "nil")
                  t :na)))
-        (add-to-list 'ac-nrepl-compliment-core-required (cons t-session status))
+        (add-to-list 'ac-cider-compliment-core-required (cons t-session status))
         status))))
 
-(defun ac-nrepl-compliment-available-p ()
+(defun ac-cider-compliment-available-p ()
   "Return t if nrepl is available for completion, otherwise nil."
-  (when (eq (ac-nrepl-compliment-require-core) t)
+  (when (eq (ac-cider-compliment-require-core) t)
     t))
 
-(defun ac-nrepl-compliment-sync-eval (clj)
+(defun ac-cider-compliment-sync-eval (clj)
   "Synchronously evaluate CLJ.
 Result is a plist, as returned from `nrepl-send-string-sync'."
   (nrepl-send-string-sync clj (cider-current-ns) (nrepl-current-tooling-session)))
 
-(defun ac-nrepl-compliment-candidates* (clj)
+(defun ac-cider-compliment-candidates* (clj)
   "Return completion candidates produced by evaluating CLJ."
-  (when (eq (ac-nrepl-compliment-require-core) t)
-    (let* ((response (plist-get (ac-nrepl-compliment-sync-eval clj) :value)))
+  (when (eq (ac-cider-compliment-require-core) t)
+    (let* ((response (plist-get (ac-cider-compliment-sync-eval clj) :value)))
       (when response
         (car (read-from-string response))))))
 
-(defvar ac-nrepl-compliment-last-context nil)
+(defvar ac-cider-compliment-last-context nil)
 
-(defun ac-nrepl-compliment-get-context-at-point ()
+(defun ac-cider-compliment-get-context-at-point ()
   "Extract the context at point. If point is not inside the list,
 returns nil; otherwise return top-level form, with symbol at
 point replaced by __prefix__."
@@ -115,7 +113,7 @@ point replaced by __prefix__."
             (user-error nil)))
     (save-excursion
       (let* ((pref-end (point))
-             (pref-start (ac-nrepl-compliment-symbol-start-pos))
+             (pref-start (ac-cider-compliment-symbol-start-pos))
              (context (cider-defun-at-point))
              (_ (beginning-of-defun))
              (expr-start (point)))
@@ -127,31 +125,31 @@ point replaced by __prefix__."
                   "__prefix__"
                   (substring context (- pref-end expr-start)))))))))
 
-(defun ac-nrepl-compliment-get-context ()
-  (let ((context (ac-nrepl-compliment-get-context-at-point)))
-    (if (string= ac-nrepl-compliment-last-context context)
+(defun ac-cider-compliment-get-context ()
+  (let ((context (ac-cider-compliment-get-context-at-point)))
+    (if (string= ac-cider-compliment-last-context context)
         ":same"
-      (setq ac-nrepl-compliment-last-context context)
+      (setq ac-cider-compliment-last-context context)
       context)))
 
-(defun ac-nrepl-compliment-candidates-everything ()
+(defun ac-cider-compliment-candidates-everything ()
   "Return all candidates for a symbol at point."
-  (setq ac-nrepl-compliment-documentation-cache nil)
-  (ac-nrepl-compliment-candidates*
+  (setq ac-cider-compliment-documentation-cache nil)
+  (ac-cider-compliment-candidates*
    (concat "(compliment.core/completions \"" ac-prefix "\" "
-           (let ((context (ac-nrepl-compliment-get-context)))
+           (let ((context (ac-cider-compliment-get-context)))
              (if context
                  (concat "'" context)
                "nil")) ")")))
 
-(defvar ac-nrepl-compliment-documentation-cache '())
+(defvar ac-cider-compliment-documentation-cache '())
 
-(defun ac-nrepl-compliment-documentation (symbol)
+(defun ac-cider-compliment-documentation (symbol)
   "Return documentation for the given SYMBOL, if available.
 Caches fetched documentation for the current completion call."
   (when symbol
     (let ((cached-doc (assoc (substring-no-properties symbol)
-                             ac-nrepl-compliment-documentation-cache)))
+                             ac-cider-compliment-documentation-cache)))
       (if cached-doc
           (cdr cached-doc)
         (let* ((doc
@@ -162,78 +160,78 @@ Caches fetched documentation for the current completion call."
                    "\"$" ""
                    (replace-regexp-in-string
                     "^\"" ""
-                    (plist-get (ac-nrepl-compliment-sync-eval
+                    (plist-get (ac-cider-compliment-sync-eval
                                 (format "(compliment.core/documentation \"%s\")" symbol))
                                :value))))))
                (doc (if (string= "\"\"" doc)
                         "No documentation available."
                       doc)))
-          (add-to-list 'ac-nrepl-compliment-documentation-cache
+          (add-to-list 'ac-cider-compliment-documentation-cache
                        (cons (substring-no-properties symbol) doc))
           doc)))))
 
-(defun ac-nrepl-compliment-symbol-start-pos ()
+(defun ac-cider-compliment-symbol-start-pos ()
   "Find the starting position of the symbol at point, unless inside a string."
   (let ((sap (symbol-at-point)))
     (when (and sap (not (in-string-p)))
       (car (bounds-of-thing-at-point 'symbol)))))
 
-(defun ac-nrepl-compliment-match-everything (prefix candidates)
+(defun ac-cider-compliment-match-everything (prefix candidates)
   candidates)
 
 ;;;###autoload
-(defface ac-nrepl-compliment-candidate-face
+(defface ac-cider-compliment-candidate-face
   '((t (:inherit ac-candidate-face)))
   "Face for nrepl candidates."
   :group 'auto-complete)
 
 ;;;###autoload
-(defface ac-nrepl-compliment-selection-face
+(defface ac-cider-compliment-selection-face
   '((t (:inherit ac-selection-face)))
   "Face for the nrepl selected candidate."
   :group 'auto-complete)
 
 ;;;###autoload
-(defconst ac-nrepl-compliment-source-defaults
-  '((available . ac-nrepl-compliment-available-p)
-    (candidate-face . ac-nrepl-compliment-candidate-face)
-    (selection-face . ac-nrepl-compliment-selection-face)
-    (prefix . ac-nrepl-compliment-symbol-start-pos)
-    (match . ac-nrepl-compliment-match-everything)
-    (document . ac-nrepl-compliment-documentation)
+(defconst ac-cider-compliment-source-defaults
+  '((available . ac-cider-compliment-available-p)
+    (candidate-face . ac-cider-compliment-candidate-face)
+    (selection-face . ac-cider-compliment-selection-face)
+    (prefix . ac-cider-compliment-symbol-start-pos)
+    (match . ac-cider-compliment-match-everything)
+    (document . ac-cider-compliment-documentation)
     (cache))
   "Defaults common to the various completion sources.")
 
 ;;;###autoload
 (defvar ac-source-compliment-everything
   (append
-   '((candidates . ac-nrepl-compliment-candidates-everything)
+   '((candidates . ac-cider-compliment-candidates-everything)
      (symbol . "v"))
-   ac-nrepl-compliment-source-defaults)
+   ac-cider-compliment-source-defaults)
   "Auto-complete source for nrepl var completion.")
 
 ;;;###autoload
-(defun ac-nrepl-compliment-setup ()
+(defun ac-cider-compliment-setup ()
   "Add the nrepl completion source to the front of `ac-sources'.
 This affects only the current buffer."
   (interactive)
   (add-to-list 'ac-sources 'ac-source-compliment-everything))
 
 ;;;###autoload
-(defun ac-nrepl-compliment-popup-doc ()
+(defun ac-cider-compliment-popup-doc ()
   "A popup alternative to `nrepl-doc'."
   (interactive)
-  (popup-tip (ac-nrepl-compliment-documentation (symbol-name (symbol-at-point)))
-             :point (ac-nrepl-compliment-symbol-start-pos)
+  (popup-tip (ac-cider-compliment-documentation (symbol-name (symbol-at-point)))
+             :point (ac-cider-compliment-symbol-start-pos)
              :around t
              :scroll-bar t
              :margin t))
 
-(provide 'ac-nrepl-compliment)
+(provide 'ac-cider-compliment)
 
 ;; Local Variables:
 ;; coding: utf-8
 ;; eval: (checkdoc-minor-mode 1)
 ;; End:
 
-;;; ac-nrepl-compliment.el ends here
+;;; ac-cider-compliment.el ends here
